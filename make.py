4cb50011cd60709a04610b0cd1f3dcd15954fad7
Rp7rf <westhouse51@gmail.com>
2017-10-06 11:12:42 +0900

    画像の中央寄せ&罫線の追加

--- make.py
+++ make.py
@@ -7,22 +7,20 @@ from reportlab.lib.pagesizes import letter, landscape
 import reportlab.lib.colors as color
 from PIL import Image
 
-
 def make(filename="pdftest"):
     pdf_canvas = set_info(filename)
-    print_string(pdf_canvas)
-    print_figure(pdf_canvas)
-    print_line(pdf_canvas)
+    # print_string(pdf_canvas)
     print_image(pdf_canvas)
+    print_box(pdf_canvas)
     pdf_canvas.save()
 
 
 # 初期設定
 def set_info(filename):
     # print(letter) # height, width
-    pdf_canvas = canvas.Canvas("./{0}.pdf".format(filename), bottomup=False, pagesize=landscape(letter))  # 原点は左上
+    pdf_canvas = canvas.Canvas("./{0}.pdf".format(filename), bottomup=False, pagesize=letter)  # 原点は左上
         
-    pdf_canvas.setAuthor("ともっくす")
+    pdf_canvas.setAuthor("しげる")
     pdf_canvas.setTitle("pythonを使ってpdf_canvasを生成する")
     pdf_canvas.setSubject("reportlab")
     
@@ -43,46 +41,28 @@ def print_string(pdf_canvas):
     pdf_canvas.setFont("HeiseiMin-W3", 30)
     pdf_canvas.drawString(300, 100, "明朝体をサイズ30で")
 
+def print_box(pdf_canvas):
+  # 普通の線
+  step = 40
+  pdf_canvas.setStrokeColor(color.blue)
+  for i in range(26,586+step,step):
+  # for i in range(6,606+step,step):
+    pdf_canvas.line(i, 360, i, 760)
+  for i in range(360,760+step,step):
+    # pdf_canvas.line(6, i, 606, i)
+    pdf_canvas.line(26, i, 586, i)
 
-# 図形
-def print_figure(pdf_canvas):
-    # 枠線のみの四角
-    pdf_canvas.rect(50, 150, 200, 250)
-
-    # 青色の円
-    pdf_canvas.setFillColor(color.blue)
-    pdf_canvas.circle(400, 350, 50, stroke=False, fill=True)
-
-
-# 線
-def print_line(pdf_canvas):
-    # 普通の線
-    pdf_canvas.line(50, 450, 500, 450)
-
-    # 赤い太い線
-    pdf_canvas.setStrokeColor(color.red)
-    pdf_canvas.setLineWidth(10)
-    pdf_canvas.line(100, 500, 550, 500)
-
-    # 破線
-    pdf_canvas.setStrokeColor(color.black)
-    pdf_canvas.setLineWidth(5)
-    pdf_canvas.setDash([2, 8, 5, 10])
-    pdf_canvas.line(150, 550, 600, 550)
-
-    # 複数の線
-    pdf_canvas.setLineWidth(1)
-    pdf_canvas.setDash([])
-    lines = [(100, 650, 200, 750), (200, 750, 300, 650), (300, 650, 300, 750), (100, 700, 400, 700)]
-    pdf_canvas.lines(lines)
-
-
+# 画像
 def print_image(pdf_canvas):
     image = Image.open('./img/panda.jpeg')
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    pdf_canvas.drawInlineImage(image,0,-image.size[1])
-
-    
+    print(letter)
+    # x,y,width,height
+    width = 552
+    height = 276
+    height_margin = 360/2 - height/2
+    print(image.size)
+    pdf_canvas.drawInlineImage(image,letter[0]/2-width/2,height_margin-height, width, height)
 
 if __name__ == '__main__':
     make()
