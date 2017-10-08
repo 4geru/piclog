d4196d3a145f17b17a86840334e307066fa3a293
4geru sakisaka <westhouse51@gmail.com>
2017-10-08 07:46:45 +0000

    rename image_search_api.py -> make_sentence00.py

--- make_sentence00.py
+++ make_sentence00.py
@@ -14,7 +14,7 @@ for w in range(0,5) :
   
   while True:
     print('<< retry >> ')
-    payload = {'apikey': '_api_key_', 'previous_description': prev_sentence}
+    payload = {'apikey': 'EtEj5GxZF4uyobFKkoyfo3h7UKCONn2I', 'previous_description': prev_sentence}
     json = requests.get('https://api.a3rt.recruit-tech.co.jp/text_suggest/v2/predict', params=payload).json()
     
     # 次のワードの瀕死を抽出
@@ -45,7 +45,7 @@ for w in range(0,5) :
     
     print(next_words + words[index+1])
   
-    payload = {'apikey': '_api_key_', 'sentence': next_words + words[index+1]}
+    payload = {'apikey': 'a6WEeoq1GLUbSsYL10BHJB4QsNNS8SH1', 'sentence': next_words + words[index+1]}
     json = requests.get('https://api.a3rt.recruit-tech.co.jp/proofreading/v1/typo', params=payload).json()
     if json['status'] == 0 or check_sentence == 0:
       break
@@ -62,7 +62,7 @@ for w in range(0,5) :
   print(next_words + words[index+1])
   
 # 文末に文字を追加
-payload = {'apikey': '_api_key_', 'previous_description': prev_sentence}
+payload = {'apikey': 'EtEj5GxZF4uyobFKkoyfo3h7UKCONn2I', 'previous_description': prev_sentence}
 r = requests.get('https://api.a3rt.recruit-tech.co.jp/text_suggest/v2/predict', params=payload)
 json = r.json()
 print(prev_sentence + json['suggestion'][0])
