81be6abc4760143410f9ae9f22947f485c8cf844
4geru sakisaka <westhouse51@gmail.com>
2017-10-09 08:44:50 +0000

    並列数を配列の数に変更

--- translate.py
+++ translate.py
@@ -1,16 +1,16 @@
 
 from multiprocessing import Pool
 import requests, json
-
+import multiprocessing as multi
+import time
 def translate(word): 
     payload = {'from': 'en', 'dest': 'ja', 'format': 'json', 'phrase': word, 'pretty': 'true'}
     r = requests.get('https://glosbe.com/gapi/translate', params=payload)
     japanese = r.json()['tuc'][0]['phrase']['text']
-    print(japanese)
     return japanese
     
 def translate_array(english_words):
-    p = Pool(16)
+    p = Pool(len(english_words))
     japanese_words = p.map( translate, english_words )
+    p.close()
     return japanese_words
-# print(japanese_words)
